9557a4c4f9fe5dec8ef74b3ac439ef42661164e2
Tumelo4 <tmosomane4@gmail.com>
2023-11-22 08:50:11 +0200

    Header updated

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
 import React from "react";
+import logo from "@/public/Blue logistics.png"
 
 const navbar = () => {
     return (
         // Logo
+        <Image 
+            src={logo} 
+            alt="Blue Logistics"
+            width={100}
+            height={100}
+        />
     )
 }
 
