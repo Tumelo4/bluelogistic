716e6cd53e2d19f98043be335fcb2aed4ca08453
Tumelo4 <tmosomane4@gmail.com>
2023-11-28 09:34:49 +0200

    Updated Home Section

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Header from './Header/Header'
-import HomeSection from "@/components/Header/HomeSection";
+import dynamic from 'next/dynamic';
+
+const HomeSection = dynamic(() => import('./Header/HomeSection'), {
+  ssr: false,
+});
+
 const Landing = () => {
-    const globeConfig = {
-    };
   return (
       <div className=' w-full h-full'>
         <Header></Header>
