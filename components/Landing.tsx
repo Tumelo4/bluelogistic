e4ea233b938b9d2e5f3c57aa165098df8d743894
Tumelo4 <tmosomane4@gmail.com>
2023-12-07 11:03:34 +0200

    added framer motion

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,6 +1,8 @@
+'use client'
 import React from 'react'
 import Header from './Header/Header'
 import dynamic from 'next/dynamic';
+import { motion } from "framer-motion";
 
 const HomeSection = dynamic(() => import('./Header/HomeSection'), {
   ssr: false,
@@ -10,10 +12,17 @@ const Landing = () => {
   return (
       <div className=' w-full h-full'>
         <Header></Header>
-        <section className=' h-screen bg-black relative'>
-            {/* <div className=' absolute flex justify-center items-center z-10 w-screen h-screen text-white'>
+        <section className=' h-screen w-screen bg-black relative flex justify-center items-center'>
+          <div className=' absolute flex justify-center items-center z-10 text-white'>
+            <motion.div
+              initial={{ x: -1000, opacity: 0 }}
+              animate={{ x: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 1 }}
+            >  
               Blue Logistics
-            </div> */}
+            </motion.div>
+          </div>
             <HomeSection />
         </section>
       </div>
